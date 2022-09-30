303a7833b49517bb5a54eb7378bf5d996501f08a
Dong <gongan1556@naver.com>
2022-09-30 12:19:11 +0900

    Sql(SimpleJdbcInsert 방식)

--- src/main/java/kds/spring/mvc/dao/BoardDAOImpl.java
+++ src/main/java/kds/spring/mvc/dao/BoardDAOImpl.java
@@ -2,8 +2,10 @@ package kds.spring.mvc.dao;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -11,25 +13,35 @@ import kds.spring.mvc.vo.BoardVO;
 
 @Repository("bdao")
 public class BoardDAOImpl implements BoardDAO{
-
-	private JdbcTemplate jdbcTemplete;
+	
+//	private JdbcTemplate jdbcTemplete;
 	private SimpleJdbcInsert simpleInsert;
 	
 	public BoardDAOImpl(DataSource datasource) {
-		simpleInsert = new SimpleJdbcInsert(datasource);
+		simpleInsert = new SimpleJdbcInsert(datasource)
+				.withTableName("board")
+				.usingColumns("title","userid","contents");
 	}
 	
+	
+//	@Override
+//	public int insertBoard(BoardVO bvo) {
+//		//보안상 끊어서
+//		String sql = " insert into board"
+//				+"(title, userid, contents)"
+//				+ " values(?,?,?)";
+//		Object[] params = new Object[] {
+//				bvo.getTitle(), bvo.getUserid(),
+//				bvo.getContents()
+//		};
+//		
+//		return jdbcTemplete.update(sql,params);
+//	}
 	@Override
 	public int insertBoard(BoardVO bvo) {
-		//보안상 끊어서
-		String sql = " insert into board"
-				+"(title, userid, contents)"
-				+ " values(?,?,?)";
-		Object[] params = new Object[] {
-				bvo.getTitle(), bvo.getUserid(),
-				bvo.getContents()
-		};
+		SqlParameterSource params = 
+				new BeanPropertySqlParameterSource(bvo);
 		
-		return jdbcTemplete.update(sql,params);
+		return simpleInsert.execute(params);
 	}
 }
--- src/main/java/kds/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/kds/spring/mvc/dao/MemberDAOImpl.java
@@ -1,30 +1,49 @@
 package kds.spring.mvc.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import kds.spring.mvc.vo.BoardVO;
 import kds.spring.mvc.vo.MemberVO;
 
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
 
-	@Autowired
-	private JdbcTemplate jdbcTemplete;
+//	@Autowired
+//	private JdbcTemplate jdbcTemplete;
+	private SimpleJdbcInsert simpleInsert;
+	
+	public MemberDAOImpl(DataSource datasource) {
+		simpleInsert = new SimpleJdbcInsert(datasource)
+				.withTableName("member")
+				.usingColumns("userid","passwd","name","email");
+	}
 	
 	
-	@Override
+//	@Override
+//	public int insertMember(MemberVO mvo) {
+//		//보안상 끊어서
+//		String sql = " insert into member"
+//				+"(userid, passwd, name, email)"
+//				+ " values(?,?,?,?)";
+//		Object[] params = new Object[] {
+//				mvo.getUserid(), mvo.getPasswd(),
+//				mvo.getName(),mvo.getEmail()
+//		};
+//		
+//		return jdbcTemplete.update(sql,params);
+//	}
 	public int insertMember(MemberVO mvo) {
-		//보안상 끊어서
-		String sql = " insert into member"
-				+"(userid, passwd, name, email)"
-				+ " values(?,?,?,?)";
-		Object[] params = new Object[] {
-				mvo.getUserid(), mvo.getPasswd(),
-				mvo.getName(),mvo.getEmail()
-		};
+		SqlParameterSource params = 
+				new BeanPropertySqlParameterSource(mvo);
 		
-		return jdbcTemplete.update(sql,params);
+		return simpleInsert.execute(params);
 	}
 	
 }
