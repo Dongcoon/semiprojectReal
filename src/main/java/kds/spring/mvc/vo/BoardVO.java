54a825e4c2364557bf0b29f64808fc4dcf0001d8
Dong <gongan1556@naver.com>
2022-10-07 14:55:51 +0900

    lombok 라이브러리 설치

--- src/main/java/kds/spring/mvc/vo/BoardVO.java
+++ src/main/java/kds/spring/mvc/vo/BoardVO.java
@@ -1,5 +1,12 @@
 package kds.spring.mvc.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
 public class BoardVO {
 	private int bno;
 	private String title;
@@ -7,55 +14,5 @@ public class BoardVO {
 	private String regdate;
 	private String views;
 	private String contents;
-	
-	
-	public BoardVO() {
-		super();
-	}
-	public int getBno() {
-		return bno;
-	}
-	public void setBno(int bno) {
-		this.bno = bno;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-	public String getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(String regdate) {
-		this.regdate = regdate;
-	}
-	public String getViews() {
-		return views;
-	}
-	public void setViews(String views) {
-		this.views = views;
-	}
-	public String getContents() {
-		return contents;
-	}
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-	@Override
-	public String toString() {
-		String write = "BoardVO [bno=%s, title=%s, userid=%s, regdate=%s, views=%s, contents=%s]";
-	      
-	      String result = String.format(write, bno, title, userid, regdate, views, contents);
-	      
-	      
-	      return result;
-	}
 	
 }
